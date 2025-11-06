d8276c159b7fd876988f77d87fca6b426aede363
Aaditya <aadityajn16@gmail.com>
2025-11-07 00:08:16 +0530

    First commit

---
+++ abc.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+student_dicts = {
+    "Name" : ["Aaditya", "Ankit", "Rohan", "Rahul"],
+    "IQ" : [100, 90, 80, 120],
+    "Marks" : [90, 80, 70, 95],
+    "Package" : [10, 12, 7, 15]
+}
+
+students = pd.DataFrame(student_dicts)
+students  
+
+
+
+
+
+ipl = pd.read_csv("/home/aaditya/Desktop/Python/Pandas/ipl-matches.csv")
+movies = pd.read_csv("/home/aaditya/Desktop/Python/Pandas/movies.csv")
